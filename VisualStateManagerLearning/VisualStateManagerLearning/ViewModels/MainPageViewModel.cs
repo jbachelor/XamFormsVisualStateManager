04af82e85c9c047f68b46e19c7669b4c3792fd4a
Jonathan Bachelor <19045+jbachelor@users.noreply.github.com>
2019-03-02 10:34:33 -0800

    Basic solution demonstrating two styled buttons. One uses the VisualStateManager to control the look of the button when IsEnabled is set to false. The other is left to the default behavior. What a difference!

---
+++ VisualStateManagerLearning/VisualStateManagerLearning/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Prism.Navigation;
+
+namespace VisualStateManagerLearning.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        private bool _shouldEnableButtons;
+        public bool ShouldEnableButtons
+        {
+            get { return _shouldEnableButtons; }
+            set { SetProperty(ref _shouldEnableButtons, value); }
+        }
+
+        private string _noVSMButtonText;
+        public string NoVSMButtonText
+        {
+            get { return _noVSMButtonText; }
+            set { SetProperty(ref _noVSMButtonText, value); }
+        }
+
+        private string _VSMButtonText;
+        public string VSMButtonText
+        {
+            get { return _VSMButtonText; }
+            set { SetProperty(ref _VSMButtonText, value); }
+        }
+
+        public MainPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            Title = "Visual States";
+            VSMButtonText = "VISUAL STATES";
+            NoVSMButtonText = "DEFAULT BEHAVIOR";
+            ShouldEnableButtons = true;
+        }
+    }
+}
