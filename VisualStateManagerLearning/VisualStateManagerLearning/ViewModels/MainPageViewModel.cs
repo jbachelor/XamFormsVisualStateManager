6e36425d8a55e846a2cb2d32444ba528def1cad0
Jonathan Bachelor <19045+jbachelor@users.noreply.github.com>
2019-03-02 11:08:02 -0800

    Adding some handlers to make for a more complete demo app.
    Commands and handlers added for each button, with debug info sent to application output view in IDE.
    Monitor the ShouldEnableButtons property to display updates in the application output view in IDE.

--- VisualStateManagerLearning/VisualStateManagerLearning/ViewModels/MainPageViewModel.cs
+++ VisualStateManagerLearning/VisualStateManagerLearning/ViewModels/MainPageViewModel.cs
@@ -1,9 +1,16 @@
-﻿using Prism.Navigation;
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using Prism.Commands;
+using Prism.Navigation;
 
 namespace VisualStateManagerLearning.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
+        public DelegateCommand VsmButtonTappedCommand { get; set; }
+        public DelegateCommand NoVsmButtonTappedCommand { get; set; }
+
         private bool _shouldEnableButtons;
         public bool ShouldEnableButtons
         {
@@ -32,6 +39,29 @@ namespace VisualStateManagerLearning.ViewModels
             VSMButtonText = "VISUAL STATES";
             NoVSMButtonText = "DEFAULT BEHAVIOR";
             ShouldEnableButtons = true;
+
+            VsmButtonTappedCommand = new DelegateCommand(OnVsmButtonTapped);
+            NoVsmButtonTappedCommand = new DelegateCommand(OnNoVsmButtonTapped);
+
+            this.PropertyChanged += OnViewModelPropertyChanged;
+        }
+
+        void OnViewModelPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if(string.Equals(e.PropertyName, nameof(ShouldEnableButtons), StringComparison.Ordinal))
+            {
+                Debug.WriteLine($"**** {this.GetType().Name}.{nameof(OnViewModelPropertyChanged)}:  {nameof(ShouldEnableButtons)} now set to {ShouldEnableButtons}.");
+            }
+        }
+
+        private void OnNoVsmButtonTapped()
+        {
+            Debug.WriteLine($"**** {this.GetType().Name}.{nameof(OnNoVsmButtonTapped)}");
+        }
+
+        private void OnVsmButtonTapped()
+        {
+            Debug.WriteLine($"**** {this.GetType().Name}.{nameof(OnVsmButtonTapped)}");
         }
     }
 }
